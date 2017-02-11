94386ef61ef7df6340f15bf2631a4df55a8139e0
Stephen Rifenburgh <Stephen@Rifenburgh.com>
2017-02-11 09:40:20 -0500

    Mario Hits bullet

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ canvas.width = width;
 canvas.height = height;
 var context = canvas.getContext('2d');
 
+
 var render = function () {
     context.fillStyle = "#5c94fc";
     mario.update();
@@ -109,6 +110,11 @@ Enemy.prototype.update = function() {
   context.fillRect(this.x, this.y, 33, 30);
   this.x -= 5;
   context.drawImage(bullet, this.x, this.y);
+  //Test Mario's range against the left side of the bullet
+  if (((mario.x + 30) === (this.x)) && ((mario.y > this.y) && (mario.y < (this.y + 30)))) {
+    console.log("Hit Bullet!");
+  }
+  // console.log(mario.x, mario.y, this.x, this.y);
 };
 
 Enemy.prototype.move = function() {
